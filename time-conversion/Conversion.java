1e84d05742e55867691753a23a09287a53ab008e
palkin-ttn <90325761+palkin-ttn@users.noreply.github.com>
2021-09-23 13:45:31 +0530

    Update Conversion.java

--- time-conversion/Conversion.java
+++ time-conversion/Conversion.java
@@ -1,1 +1,24 @@
+public class Conversion {
+    static void ConvertSec(int n) {
+        int day = n / (24 * 3600);
+        n = n % (24 * 3600);
+        int hour = n / 3600;
 
+        n %= 3600;
+        int minutes = n /60 ;
+
+        n %= 60;
+        int seconds = n;
+
+        System.out.println( day + " " + "days "
+                + hour + " " + "hours "
+                + minutes + " " + "minutes "
+                + seconds + " " + "seconds"
+        );
+    }
+    public static void main(String[] args) {
+        System.out.println(" * Given n is in seconds * ");
+        int n = 129850;
+        ConvertSec(n);
+    }
+}
