ca635ed05f73a2fed4dc556eb415f9470fae8d4d
palkin-ttn <90325761+palkin-ttn@users.noreply.github.com>
2021-09-23 13:29:20 +0530

    Update Blocks.java

--- try-catch-and-finally/Blocks.java
+++ try-catch-and-finally/Blocks.java
@@ -1,1 +1,22 @@
-
+public class Blocks {
+    public static void main(String args[]) {
+        try {
+            int a[] = new int[8];
+            a[4] = 30/0;
+            System.out.println("Try block");
+        }
+        catch(ArithmeticException e) {
+            System.out.println("Warning: ArithmeticException");
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Warning: ArrayOutOfBoundsException");
+        }
+        catch(Exception e) {
+            System.out.println("Warning: Some other Exception");
+        }
+        finally {
+            System.out.println("Finally block");
+        }
+        System.out.println("Out of try, catch and finally block...");
+    }
+}
